c3c43e0a35415f33750cc533880822506aee91db
crazyuploader <49350241+crazyuploader@users.noreply.github.com>
2020-05-11 23:48:39 +0530

    Update: script.sh

--- script.sh
+++ script.sh
@@ -5,4 +5,11 @@ echo ""
 git clone https://${GITHUB_REF} temp
 cd temp
 git fetch https://github.com/osm0sis/AnyKernel3.git
+echo ""
 git merge FETCH_HEAD
+git config user.email "49350241+crazyuploader@users.noreply.github.com"
+git config user.name "crazyuploader"
+echo ""
+git push https://crazyuploader:"${GITHUB_TOKEN}"@"${GH_REF}" HEAD:master
+echo ""
+echo "Done"
