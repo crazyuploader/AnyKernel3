885acf75686dd324c551b4911594eaf47140ecf0
crazyuploader <49350241+crazyuploader@users.noreply.github.com>
2020-05-11 23:43:57 +0530

    Update: script.sh

--- script.sh
+++ script.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
 
-echo "Doing Nothing"
+echo "Pulling Anykernel3 from my GitHub..."
+echo ""
+git clone https://${GITHUB_REF} temp
+cd temp
+git fetch https://github.com/osm0sis/AnyKernel3.git
+git merge FETCH_HEAD
