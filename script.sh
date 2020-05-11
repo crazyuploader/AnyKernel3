710a85262b66cde049f9e85f0924e40453852050
Jugal Kishore <49350241+crazyuploader@users.noreply.github.com>
2020-05-11 23:59:30 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -4,13 +4,12 @@ echo "Pulling Anykernel3 from my GitHub..."
 echo ""
 git clone https://${GITHUB_REF} temp
 cd temp
-git reset --hard HEAD~1
 git fetch https://github.com/osm0sis/AnyKernel3.git
 echo ""
 git config user.email "49350241+crazyuploader@users.noreply.github.com"
 git config user.name "crazyuploader"
 git merge FETCH_HEAD
 echo ""
-git push https://crazyuploader:"${GITHUB_TOKEN}"@"${GITHUB_REF}" HEAD:master --force
+git push https://crazyuploader:"${GITHUB_TOKEN}"@"${GITHUB_REF}" HEAD:master
 echo ""
 echo "Done"
